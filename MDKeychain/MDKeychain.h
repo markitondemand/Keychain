986424891403244d2489723024c8adc4e95eaee5
Michael Leber <michael.leber@markit.com>
2017-07-12 10:21:35 -0600

    Initial Commit

---
+++ MDKeychain/MDKeychain.h
@@ -0,0 +1,19 @@
+//
+//  MDKeychain.h
+//  MDKeychain
+//
+//  Created by Michael Leber on 7/12/17.
+//  Copyright © 2017 Markit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MDKeychain.
+FOUNDATION_EXPORT double MDKeychainVersionNumber;
+
+//! Project version string for MDKeychain.
+FOUNDATION_EXPORT const unsigned char MDKeychainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MDKeychain/PublicHeader.h>
+
+
